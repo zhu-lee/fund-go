621ef57ac281267ddad7fe45d07ae5126235594c
zhu.li <zhu.li@mtime.com>
2019-05-19 12:59:39 +0800

    s

--- com.lee/config-web/resp/write.go
+++ com.lee/config-web/resp/write.go
@@ -2,6 +2,6 @@ package resp
 
 import "com.lee/fund/web"
 
-func WritePage(ctx *web.Context,name string,args interface{})  {
-	ctx.WriteTemplate(name, args)
+func WritePage(ctx *web.Context, name string, args interface{}) {
+	ctx.WriteTemplate("pages/"+name, args)
 }
